1e8ba91889c9e1c32d3fc2981d0bcd99f5bb1446
Piyush Wankar <pwankar@dgfuturetech.lan>
2024-05-22 09:16:40 +0530

    Serializing student.csv file

---
+++ ASS-05-v1-StudentCSV_serialization/src/main/java/com/wp/StudentCSV_serialization/entities/Student.java
@@ -0,0 +1,32 @@
+package com.wp.StudentCSV_serialization.entities;
+
+import java.io.Serializable;
+
+public class Student implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int rno;
+	private String name;
+	private double per;
+	
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPer() {
+		return per;
+	}
+	public void setPer(double per) {
+		this.per = per;
+	}
+	
+}
