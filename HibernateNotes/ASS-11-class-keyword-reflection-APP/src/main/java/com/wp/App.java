be71433b5466b6a4914bff36b80d03c2b77b42fa
Piyush Wankar <161890058+wpdev2001@users.noreply.github.com>
2024-06-08 09:08:57 +0900

    Update App.java

--- HibernateNotes/ASS-11-class-keyword-reflection-APP/src/main/java/com/wp/App.java
+++ HibernateNotes/ASS-11-class-keyword-reflection-APP/src/main/java/com/wp/App.java
@@ -28,6 +28,65 @@ public class App
         {
             System.out.println(m);
         }
-
+        
     }
 }
+
+
+//-----------------------------------------------------------------------------
+// import java.lang.reflect.Field;
+
+// class Person {
+//     private String name;
+//     private int age;
+
+//     public Person(String name, int age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     // Getters (not strictly necessary for reflection, but useful for testing)
+//     public String getName() {
+//         return name;
+//     }
+
+//     public int getAge() {
+//         return age;
+//     }
+// }
+
+// public class ReflectionExample {
+//     public static void main(String[] args) {
+//         try {
+//             // Create an instance of the Person class
+//             Person person = new Person("Alice", 30);
+
+//             // Access the private field 'name'
+//             Field nameField = Person.class.getDeclaredField("name");
+//             nameField.setAccessible(true); // Allow access to the private field
+
+//             // Get the value of the 'name' field
+//             String nameValue = (String) nameField.get(person);
+//             System.out.println("Name: " + nameValue);
+
+//             // Set a new value for the 'name' field
+//             nameField.set(person, "Bob");
+//             System.out.println("Updated Name: " + person.getName());
+
+//             // Access the private field 'age'
+//             Field ageField = Person.class.getDeclaredField("age");
+//             ageField.setAccessible(true); // Allow access to the private field
+
+//             // Get the value of the 'age' field
+//             int ageValue = (int) ageField.get(person);
+//             System.out.println("Age: " + ageValue);
+
+//             // Set a new value for the 'age' field
+//             ageField.set(person, 35);
+//             System.out.println("Updated Age: " + person.getAge());
+
+//         } catch (NoSuchFieldException | IllegalAccessException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
