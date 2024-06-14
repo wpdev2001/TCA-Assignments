d22a0ab2cc88ad1b26a9beed2253d11841205ecf
WP <priyanshudevilliers@gmail.com>
2024-06-14 23:58:05 +0000

    Hibernate app using myconfig.properties

---
+++ ASS-13-HIB-App-Without-CFG_File-Parsing-error/src/main/java/com/wp/App.java
@@ -0,0 +1,54 @@
+package com.wp;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.wp.entities.Student;
+
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        Configuration configuration = new Configuration();
+        //NO need of configure method bcz by default when the configuration constructor is called it will search for hibernate.properties file
+        
+        //Giving the mapping file details
+        //Earlier we used to give those details within configuration file. So, can't we pass those details within hibernate.properties file?
+        FileInputStream fis = new FileInputStream("src/main/resources/myconfig.properties");
+        Properties prop = new Properties();
+        prop.load(fis);
+        configuration.setProperties(prop);
+        
+        configuration.addAnnotatedClass(Student.class);
+        
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        
+        Session session = sessionFactory.openSession();
+        
+        Transaction txn = session.beginTransaction();
+        
+        Student sob = new Student();
+        sob.setRno(101);
+        sob.setName("AAA");
+        sob.setPer(88.0);
+        
+        session.save(sob);
+        txn.commit();
+        
+        System.out.println("First HIB application on AWS EC2 instance!!!");
+        
+        session.close();
+        sessionFactory.close();
+        
+        
+        
+    }
+}
